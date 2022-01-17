764f80627b50d6cd4a5f786fd03247f73ad06c8f
Manuel Exposito <manuelexposito4d@gmail.com>
2022-01-17 20:33:53 +0100

    ejercicio listados horizontales

---
+++ ud0504_starwars/lib/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Styles{
+
+
+//FONTS
+
+static TextStyle textName = TextStyle(fontSize: 15, fontFamily: 'Soloist');
+
+static TextStyle textTitle = TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w800, color: Colors.white);
+        
+}
