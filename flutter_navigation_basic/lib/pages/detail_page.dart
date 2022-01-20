8900e1bd62207a8a0f2399bc62c36cfbf9e5a1e5
ManuelExposito <manuelexposito4d@gmail.com>
2022-01-20 13:28:14 +0100

    apuntes navigation

---
+++ flutter_navigation_basic/lib/pages/detail_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
